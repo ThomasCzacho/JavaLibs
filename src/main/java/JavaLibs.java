593258714b85aa988b84cc6fdcb8a8732702bc6e
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-22 09:38:34 -0400

    9:38am Commit

--- src/main/java/JavaLibs.java
+++ src/main/java/JavaLibs.java
@@ -16,7 +16,7 @@ Scanner sc = new Scanner(System.in);
         System.out.println("^~^~^|_|_[X]_|____|~^~^~^~^~^~^~^~`\"\"`\"\"))\"\"`\"`\"\"\"\"`~^~^");
         System.out.println("Art by Joan Stark: ASCII Art Archive \n  ");
 
-
+// prompt asked for coded version of madlibs, I decided to make it into a short horror story
 
           System.out.println("==(1/10)== Declare First Name. Hit Enter to Confirm. ====");
         String firstName = sc.next();
@@ -78,13 +78,13 @@ Scanner sc = new Scanner(System.in);
                         "reading off  a mental check-list.\n\n \" Liscense and Registration" +
                         " Please \".\n\n He is clutching his right side, His shirt has 3 small " +
                         "holes that you can see from where he is holding, \n yet he acts fine none the less. \n\n  " +
-                        "\" You know- You look all to familiar, you happen to" +
+                        "\" You know- You look all too familiar, you happen to" +
                         " know Mr."+lastName+" by any chance? \" \n\n" +
                         "The Officer is talking about your uncle Huel, you explain how" +
                         " you were on your way right now to go meet him. \n\n" +
                         "\" Oh, is that so, Well, I'll leave you off on a warning, tell " +
                         "Huey I send my regards. \" \n\n Like that the officer returns to his" +
-                        " car and pulls away infront of you, you note his license plate, \n \"" +
+                        " car and pulls away infront of you, you glance at his license plate, \n \"" +
                         ""+number+number1+number+"2L"+number+"3H"+number+"\". You continue on your drive.\n");
         System.out.println("========  Type any Letter and Hit Enter to Continue. ====");
         String u = sc.next();
@@ -94,22 +94,31 @@ Scanner sc = new Scanner(System.in);
                 "does the mansion's full size dwell on you, seemingly towering over you, yet being no \n" +
                 "taller than the buildings you traverse through on main street. You find yourself now at \n" +
                 "the front door. The large iron door knocker intimidates you to the point where you perfer to \n" +
-                "knock instead. When knocking on the door you accidentally move the door slightly, realizing" +
+                "knock instead. When knocking on the door you accidentally move the door slightly, realizing " +
                 "it's unlocked.\n");
         System.out.println("========  Type any Letter and Hit Enter to Continue. ====");
         String y = sc.next();
-        System.out.println("Letting yourself in, you finally spot Uncle Huel cradling his "+animal+", Stimpy, On his Sofa.");
+        System.out.println("Letting yourself in, you finally spot Uncle Huel cradling his "+animal+", Stimpy, On his Sofa.\n\n" +
+                "\" "+firstName+", Come in, fancy seeing you here! \" \n\n You Explain to Uncle Huel that he invited you, \n" +
+                "but he shrugs off his confusing comment, denoting it because of his old age.\n He goes on making conversation about family, the farm. \n" +
+                "he mentions how his new delivery driver is still often late and forgets his address. \n\n" +
+                "\" Guess the orchard sign is too small for him, even so, you figure he'd remember the way by now! \" He jokes, \n\n You leave the living room and  enter his dining room where" +
+                "you realize the table is set, \n yet there is no plates or silverware, this seems odd but you take a seat without too much thought.\n" +
+                "while you continue to talk your eyes drift to behind Uncle Huel, to his grand china cabinet, \nontop" +
+                "sits a small white urn, with the name \"Stimpy\" spelled out. \n");
+
                 System.out.println("========  Type any Letter and Hit Enter to Continue. ====");
                 String e = sc.next();
-                System.out.println("You're now very confused because you also notice Stimpy's Urn on the shelf behind him, you remember Stimpy was\n" +
-                "ran over by a delivery truck 4 years back. Looking back at the two you now notice that Stimpy's spine seems deformed.\n " +
+                System.out.println("You're now very confused because the same Stimpy is begging for food next to you, despite the table being \n" +
+                        " devoid of any, you now remember the reason why Uncle Huel has a new delivery driver. 4 years ago, 1990, Stimpy was ran \n" +
+                        "over by the delivery truck. Looking back down you now notice that Stimpy's spine seems deformed. Flattened even.\n " +
                 "Starting to panic you excuse yourself to go outside for a breather, the front door falls off when you touch it, now black\n" +
                 "and charred. turning around you realize the house you were just in was nothing but the burnt remnants of a house. \n" +
                 "Peering into what used to be the living room you see Uncle Huel cradling Stimpy as if everything is as normal.\n" +
                 "In confusion you run to your car before you realize you can't find the door's handle. In fact, you can't find the door.\n" +
                 "your car is warped beyond recognition, you notice the front is bent in a way that it could perfectly fit a large tree\n" +
-                "between what used to be the wheel wells.\n\n The Delivery Truck and Stimpy, the Fire and Huel, you and the tree. You Realize \n" +
-                "that you are dead.\" "+firstName+", "+lastName+", date of death: "+date+".1995, at 7:1"+number+"pm. Car found 6 miles west of "+town+", "+region+" \".\n");
+                "between what used to be the wheel wells.\n\nThe Delivery Truck and Stimpy, the Fire and Huel, you and the tree. You Realize " +
+                "that you are dead.\n\" "+firstName+", "+lastName+", date of death: "+date+".1995, at 7:1"+number+"pm. Car found "+number+" miles west of "+town+", "+region+" \".\n");
 
         System.out.println("========  Type any Letter and Hit Enter to Continue. ====");
         String t = sc.next();
